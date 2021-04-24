7ccb72b1d18b749fe8741af09b33ecedacf5ff3f
Aleksandra Balinska <balinska.aleksandra@gmail.com>
2021-04-24 16:38:38 +0200

    Skakanie po planetkach

---
+++ Boost Game Jam/Assets/Alex/SkryptyPlanetka/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 15;
+    private Vector2 moveDir;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        //moveDir = new Vector2 (Input.GetAxis("Horizontal"),0).normalized;
+        
+
+    }*/
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey("right"))
+ {
+    rb.AddForce(transform.right * speed);
+ }
+ if(Input.GetKey("left"))
+ {
+    rb.AddForce(transform.right * -1 * speed);
+ }
+ if(Input.GetKey("up"))
+ {
+    rb.AddForce(transform.up * speed*5);
+ }
+
+        //Move(); 
+       // GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + transform.TransformDirection(moveDir) *speed* Time.deltaTime); 
+    }
+
+    void Move() { 
+    float x = Input.GetAxisRaw("Horizontal"); 
+    float moveBy = x * speed; 
+    rb.velocity = new Vector2(moveBy, rb.velocity.y); 
+}
+}
+
---
+++ Boost Game Jam/Assets/Alex/SkryptyPlanetka/PlayerGravityBody.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGravityBody : MonoBehaviour {
+
+    public PlanetScript attractorPlanet;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        //GetComponent<Rigidbody2D>().GravityScale = 0;
+        //GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints.FreezeRotation;
+
+        playerTransform = transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (attractorPlanet)
+        {
+            attractorPlanet.Attract(playerTransform);
+        }
+    }
+
+        void OnTriggerEnter2D(Collider2D other)
+		{
+		if (other.tag=="Planet") {
+
+            Debug.Log("skok");
+            attractorPlanet = other.GetComponent<PlanetScript>();
+            
+        }
+
+			
+    }
+    
+}
