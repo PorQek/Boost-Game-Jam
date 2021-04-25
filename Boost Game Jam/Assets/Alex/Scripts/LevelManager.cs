8c50e8ad6072fe6a44a2db659149c4b639f450ad
Aleksandra Balinska <balinska.aleksandra@gmail.com>
2021-04-25 12:20:35 +0200

    Grafika na start + placeholder i trigger do wygranka

--- Boost Game Jam/Assets/Alex/Scripts/LevelManager.cs
+++ Boost Game Jam/Assets/Alex/Scripts/LevelManager.cs
@@ -5,10 +5,13 @@ public class LevelManager : MonoBehaviour {
 
 	public GameObject currentCheckpoint;
 	private PlayerLife player;
+	public GameObject start;
+	public float TimeToDisplay=2;
 	// Use this for initialization
 	void Start ()
 	{
-		
+
+		start.SetActive(true);
 		player = FindObjectOfType<PlayerLife>();
 
 	}
@@ -19,10 +22,17 @@ public class LevelManager : MonoBehaviour {
 			RespawnPlayer ();
 		}
 
+		TimeToDisplay=TimeToDisplay- Time.deltaTime;
+
 		//if (Input.GetKey("escape")) 
 		//	{
 		//	Application.LoadLevel("Menu");
 		//}
+if (TimeToDisplay<=0)
+{
+	start.SetActive(false);
+}
+
 	}
 
 	public void RespawnPlayer()
